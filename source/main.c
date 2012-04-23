8f675cf0314272ee1dd1c4e1773c7b7808f13679
Nicolas JACQUES <jacques.nicolas37@gmail.com>
2012-04-23 17:04:06 +0200

    [Pacman] - Installation
    - Ajout du projet vierge

---
+++ source/main.c
@@ -0,0 +1,23 @@
+/**
+ * @file main.c
+ * @brief Projet Polytech Pacman sur Nintendo DS
+ * @author Lendry CELERON & Nicolas JACQUES
+ * @version 1.0
+ * @date 23 avril 2012
+ *
+ * Pacman adapté sur plateforme Nintendo DS.
+ *
+ */
+
+
+/**
+ * @fn int main( void )
+ * @brief Entrée principale du programme.
+ *
+ * @return 0 si arrêt normal du programme
+ */
+
+int main( void )
+{
+	return 0;
+}
